4f95531f3b424051d6c800209b2ee802d032bb19
Abeera1234 <31432329+Abeera1234@users.noreply.github.com>
2017-10-03 09:35:15 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Please enter your number: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int factorial = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine(factorial);
+           
+            Console.ReadKey();
+        }
+    }
+}
